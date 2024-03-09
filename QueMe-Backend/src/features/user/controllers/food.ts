c3d26e834ac0a57de7740b97a92953f14adc924b
ChoMaker <thanapong.kam@ku.th>
2024-03-09 20:23:27 +0700

    Edit API getfoods

--- QueMe-Backend/src/features/user/controllers/food.ts
+++ QueMe-Backend/src/features/user/controllers/food.ts
@@ -4,7 +4,9 @@ import { SearchFood } from "../../../models/searchFood";
 import { FoodService } from "../services/food";
 
 export namespace FoodController {
-  export const getFoods = async (res: Response) => {
+  export const getFoods = async (req: Request, res: Response) => {
+    const body = req.body;
+
     try {
       const result = await FoodService.getFoods();
       return res.status(200).json({

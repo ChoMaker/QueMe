586de88a9741877824110c4f04ed26815e419a64
ChoMaker <thanapong.kam@ku.th>
2024-03-10 21:43:31 +0700

    Edit API

--- QueMe-Backend/src/features/user/controllers/order.ts
+++ QueMe-Backend/src/features/user/controllers/order.ts
@@ -2,17 +2,30 @@ import { Request, Response } from "express";
 import { ReserveOrder } from "../../../models/reserveOrder";
 import { OrderService } from "../services/order";
 
+const ReserveData: { food_id: number; que_id: number; quantity: number }[] = [];
+
 export namespace OrderController {
   export const reserveOrder = async (req: Request, res: Response) => {
-    const body = req.body as ReserveOrder;
+    const body = req.body as {
+      food_id: number;
+      que_id: number;
+      quantity: number;
+    }[];
+
+    if (!Array.isArray(body)) {
+      return res.status(400).json({
+        message: "Invalid data format. Expecting an array of orders.",
+      });
+    }
 
     try {
       const result = await OrderService.reserveOrder(body);
+      ReserveData.push(...body);
       return res.status(200).json({
         message: "Reserve order successfully",
       });
     } catch (error) {
-      return res.status(404).json({
+      return res.status(500).json({
         message: "Can't reserve order",
       });
     }
--- QueMe-Backend/src/features/user/services/order.ts
+++ QueMe-Backend/src/features/user/services/order.ts
@@ -13,21 +13,26 @@ type que_id = {
 };
 
 export namespace OrderService {
-  export const reserveOrder = async (body: ReserveOrder) => {
-    const [resultReserveOrder] = await (
-      await connection
-    ).query("SELECT * from que WHERE id=?", [body.que_id]);
-    const resultQueId = (resultReserveOrder as que_id[])[0];
+  export const reserveOrder = async (orders: ReserveOrder[]) => {
+    const resultQueArray: que_id[] = [];
 
-    if (resultQueId.id) {
-      const resultQue = await (
+    for (const order of orders) {
+      const [resultReserveOrder] = await (
         await connection
-      ).query("INSERT INTO orders (food_id,que_id,quantity) VALUES (?,?,?)", [
-        body.food_id,
-        resultQueId.id,
-        body.quantity,
-      ]);
-      return resultQue;
+      ).query("SELECT * from que WHERE id=?", [order.que_id]);
+      const resultQueId = (resultReserveOrder as que_id[])[0];
+
+      if (resultQueId.id) {
+        const resultQue = await (
+          await connection
+        ).query("INSERT INTO orders (food_id,que_id,quantity) VALUES (?,?,?)", [
+          order.food_id,
+          resultQueId.id,
+          order.quantity,
+        ]);
+
+        resultQueArray.push(resultQueId);
+      }
     }
   };
 }

48c0f192d74a1c29320b5de6eb31a10240d70310
ChoMaker <thanapong.kam@ku.th>
2024-03-16 15:27:06 +0700

    Add route API

--- QueMe-Backend/src/routes/event.ts
+++ QueMe-Backend/src/routes/event.ts
@@ -1,1 +1,5 @@
 import express, { Request, Response } from "express";
+import { EventController } from "../features/user/controllers/event";
+
+export const eventRouter = express.Router();
+eventRouter.get("/qm/getevents", EventController.getEvents);
